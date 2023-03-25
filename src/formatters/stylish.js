0e383b053e5740a97d93b07b46a7ef3ed81dd025
roman-usov <66104246+roman-usov@users.noreply.github.com>
2023-03-25 14:22:25 +0400

    refactored functions

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,36 +6,37 @@ function createObjPropertyString(key, space, val) {
   return `${key}:${space}${val}`;
 }
 
-function stylish(data) {
-  function iterate(val) {
-    return val.reduce((acc, element) => {
-      const [key, details] = Object.entries(element).flat();
-      const indentBefore = INDENT_CHAR.repeat(
-        SPACES_PER_INDENT * details.depth - SPACES_FOR_CHANGE_INFO,
-      );
-      const indentAfter = INDENT_CHAR.repeat(SPACES_PER_INDENT * details.depth);
-      const keyString = `${acc}${indentBefore}${details.status}${key}`;
-      const spaceAfterColon = details.value === '' ? '' : ' ';
-      let valueString;
+function stringIterator(val) {
+  return val.reduce((acc, element) => {
+    const [key, details] = Object.entries(element).flat();
+    const indentBefore = INDENT_CHAR.repeat(
+      SPACES_PER_INDENT * details.depth - SPACES_FOR_CHANGE_INFO,
+    );
+    const indentAfter = INDENT_CHAR.repeat(SPACES_PER_INDENT * details.depth);
+    const keyString = `${acc}${indentBefore}${details.status}${key}`;
+    const spaceAfterColon = details.value === '' ? '' : ' ';
+    let valueString;
 
-      if (details.children) {
-        const childrenString = iterate(details.children);
-        valueString = `{\n${childrenString}${indentAfter}}\n`;
-
-        return createObjPropertyString(keyString, spaceAfterColon, valueString);
-      }
-
-      valueString = `${stringify(
-        details.value,
-        INDENT_CHAR,
-        SPACES_PER_INDENT,
-        details.depth + 1,
-      )}\n`;
+    if (details.children) {
+      const childrenString = stringIterator(details.children);
+      valueString = `{\n${childrenString}${indentAfter}}\n`;
 
       return createObjPropertyString(keyString, spaceAfterColon, valueString);
-    }, '');
-  }
-  const lines = iterate(data);
+    }
+
+    valueString = `${stringify(
+      details.value,
+      INDENT_CHAR,
+      SPACES_PER_INDENT,
+      details.depth + 1,
+    )}\n`;
+
+    return createObjPropertyString(keyString, spaceAfterColon, valueString);
+  }, '');
+}
+
+function stylish(data) {
+  const lines = stringIterator(data);
 
   return `\n{\n${lines}}`;
 }
