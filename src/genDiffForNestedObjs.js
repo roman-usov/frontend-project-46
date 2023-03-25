04dc2e22806ca8afd2df493e439c69389453d869
roman-usov <66104246+roman-usov@users.noreply.github.com>
2023-03-25 14:27:59 +0400

    refactored genDiffForNestedObjs

--- src/genDiffForNestedObjs.js
+++ src/genDiffForNestedObjs.js
@@ -15,40 +15,40 @@ function createDiffObj(key, status, depth, value, children = null) {
   };
 }
 
-export default function genDiffForNestedObjs(originalObj, changedObj) {
-  function iterate(origObj, chObj, depth = 1) {
-    const uniqueKeys = sortBy(Object.keys({ ...origObj, ...chObj }));
+function diffObjIterator(origObj, chObj, depth = 1) {
+  const uniqueKeys = sortBy(Object.keys({ ...origObj, ...chObj }));
 
-    return uniqueKeys.reduce((acc, key) => {
-      const originalValue = origObj[key];
-      const changedValue = chObj[key];
+  return uniqueKeys.reduce((acc, key) => {
+    const originalValue = origObj[key];
+    const changedValue = chObj[key];
 
-      if (isObject(originalValue) && isObject(changedValue)) {
-        const children = iterate(originalValue, changedValue, depth + 1);
-        acc.push(createDiffObj(key, UNCHANGED, depth, null, children));
-        return acc;
-      }
+    if (isObject(originalValue) && isObject(changedValue)) {
+      const children = diffObjIterator(originalValue, changedValue, depth + 1);
+      acc.push(createDiffObj(key, UNCHANGED, depth, null, children));
+      return acc;
+    }
 
-      if (isEqual(originalValue, changedValue)) {
-        acc.push(createDiffObj(key, UNCHANGED, depth, originalValue));
-        return acc;
-      }
+    if (isEqual(originalValue, changedValue)) {
+      acc.push(createDiffObj(key, UNCHANGED, depth, originalValue));
+      return acc;
+    }
 
-      if (has(origObj, key) && !has(chObj, key)) {
-        acc.push(createDiffObj(key, REMOVED, depth, originalValue));
-        return acc;
-      }
-
-      if (!has(origObj, key) && has(chObj, key)) {
-        acc.push(createDiffObj(key, ADDED, depth, changedValue));
-        return acc;
-      }
-
+    if (has(origObj, key) && !has(chObj, key)) {
       acc.push(createDiffObj(key, REMOVED, depth, originalValue));
+      return acc;
+    }
+
+    if (!has(origObj, key) && has(chObj, key)) {
       acc.push(createDiffObj(key, ADDED, depth, changedValue));
       return acc;
-    }, []);
-  }
+    }
 
-  return iterate(originalObj, changedObj, 1);
+    acc.push(createDiffObj(key, REMOVED, depth, originalValue));
+    acc.push(createDiffObj(key, ADDED, depth, changedValue));
+    return acc;
+  }, []);
+}
+
+export default function genDiffForNestedObjs(originalObj, changedObj) {
+  return diffObjIterator(originalObj, changedObj, 1);
 }
